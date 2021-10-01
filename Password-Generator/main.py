3632c6dca022ca4d2f55218ee127f2e6900ca13f
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2021-10-01 22:30:39 +0100

    Create main.py

---
+++ Password-Generator/main.py
@@ -0,0 +1,13 @@
+# Made by Stephen Flynn
+
+import random
+from string import digits
+from string import punctuation
+from string import ascii_letters
+
+symbols = ascii_letters + digits + punctuation
+secure_random = random.SystemRandom()
+password = ''.join(secure_random.choice(symbols)
+                   for i in range(20))
+
+print(password)
